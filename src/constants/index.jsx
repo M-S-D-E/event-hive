52fcf1c14390cc31c19044e1302a07f4905396f6
Your Name <bettylizzie25@gmail.com>
2024-07-10 10:26:02 +0000

    React Router Setup

---
+++ src/constants/index.jsx
@@ -0,0 +1,36 @@
+const K={
+    NAVLINKS: [
+      {
+        name: "Landing",
+        path: "/",
+      },
+      {
+        name: "SignUp",
+        path: "/sign-up",
+      },
+      {
+        name: "SignIn",
+        path: "/sign-in",
+      },
+      {
+        name: "Event",
+        path: "/event",
+      },
+      {
+        name: "CreateEvent",
+        path: "/create-event",
+      },
+      {
+        name: "College",
+        path: "/college",
+      },
+      {
+        name: "404",
+        path: "/404",
+      },
+      
+    ],
+  
+   
+  };
+  export default K;
